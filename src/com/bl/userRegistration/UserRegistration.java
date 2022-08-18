8f10b0e9785082281f74d6101b74eb6840afd8d5
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-08-18 12:00:53 +0530

    Added: finding valid last name

--- src/com/bl/userRegistration/UserRegistration.java
+++ src/com/bl/userRegistration/UserRegistration.java
@@ -7,15 +7,24 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration Program!");
-
-		//validating First name
 		Scanner input = new Scanner(System.in);
+		
+		// validating First name
 		System.out.println("Enter your First Name should have one Caps and 3 minimum characters :");
 		String firstName = input.next();
 		if (firstName(firstName))
 			System.out.println("First Name is : " + firstName);
 		else
 			System.out.println("Entered First Name is invalid.Please check it! ");
+		
+		//validating last name
+		System.out.println("Enter your Last Name should have one Caps and 3 minimum characters :");
+		String last_Name = input.next();
+		if (lastName(last_Name))
+			System.out.println("Last Name is : " + last_Name);
+		else
+			System.out.println("Entered Last Name is invalid.Please check it! ");
+		
 
 	}
 
@@ -23,9 +32,9 @@ public class UserRegistration {
 	public static boolean firstName(String firstName) {
 		return firstName.matches("[A-Z]{1}[a-z]{3,}+$");
 	}
-	
+
 	// validate last name
-		public static boolean lastName(String lastName) {
-			return lastName.matches("[A-Z][a-z]*");
-		}
+	public static boolean lastName(String last_Name) {
+		return last_Name.matches("[A-Z][a-z]*");
+	}
 }
